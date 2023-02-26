68a4d7fdb36652052a70744c1285c4850bbb67a2
Xena Ship <shipxena@gmail.com>
2023-02-26 19:23:45 +0300

    issue2

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,10 +4,17 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == 'test'
-    assert arrs.get([3, 48, 5], -10) == None
+    assert arrs.get([3, 48, 5], -10) is None
+    assert arrs.get([1, 2, 3, 4], 10) is None
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-
+    assert arrs.my_slice([1, 2, 3, 4], -1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 3], -1, 1) == []
+    assert arrs.my_slice([1, 2], 1, 4) == [2]
+    assert arrs.my_slice([1, 2, 3, 4], 1, -5) == []
+    assert arrs.my_slice([1, 2, 3, 4], 0, -1) == [1, 2, 3]
+    assert arrs.my_slice([], 1, 2) == []
+    assert arrs.my_slice([1, 2, 3, 4], -10, 1) == [1]
